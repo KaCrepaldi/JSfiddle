dfb3e2509d71477de7a3b313e35a11fd28563393
Karla Crepaldi <163344451+KaCrepaldi@users.noreply.github.com>
2024-08-02 09:34:11 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function obterMedia(){
+
+var a = parseInt(document.getElementById("a").value);
+var b = parseInt(document.getElementById("b").value);
+var c = parseInt(document.getElementById("c").value);
+var d = parseInt(document.getElementById("d").value);
+var e = parseInt(document.getElementById("e").value);
+
+var media = (a + b + c + d + e) / 5;
+
+document.getElementById("resultado").innerHTML = "resultado:" + media;
+
+ console.log(a + b + c + d + e);
+}
